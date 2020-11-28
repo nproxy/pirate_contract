0dafce36a00c98acaada58464549bc178650e8c9
megaliao <micliao@tutanota.com>
2020-11-28 14:44:42 +0800

    add to rep

--- cabinet/cabinet.go
+++ cabinet/cabinet.go
@@ -8,6 +8,30 @@ type PirateEthSetting struct {
 	MinerDeposit   *big.Int
 }
 
+func (pes *PirateEthSetting)Dup() *PirateEthSetting  {
+	d:=&PirateEthSetting{
+		MinerDeposit: &big.Int{},
+		MBytesPerToken: &big.Int{},
+		PoolDeposit: &big.Int{},
+	}
+
+	if pes.PoolDeposit == nil{
+		pes.PoolDeposit = &big.Int{}
+	}
+	if pes.MinerDeposit == nil{
+		pes.MinerDeposit = &big.Int{}
+	}
+	if pes.MBytesPerToken == nil{
+		pes.MBytesPerToken = &big.Int{}
+	}
+
+	d.PoolDeposit.Add(d.PoolDeposit,pes.PoolDeposit)
+	d.MinerDeposit.Add(d.MinerDeposit,pes.MinerDeposit)
+	d.MBytesPerToken.Add(d.MBytesPerToken,pes.MBytesPerToken)
+
+	return d
+}
+
 type PirateEthUserData struct {
 	ChargeBalance *big.Int
 	TotalTraffic  *big.Int
